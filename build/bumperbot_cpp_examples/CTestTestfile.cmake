74c01842ef86f1bc7bb1d01ace39318a17b423e7
Zaid <zedsargruroh@gmail.com>
2026-08-07 17:37:56 +0200

    integrated default differential drive controller

---
+++ build/bumperbot_cpp_examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zaid/bumperbot_ws/src/bumperbot_cpp_examples
+# Build directory: /home/zaid/bumperbot_ws/src/build/bumperbot_cpp_examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
