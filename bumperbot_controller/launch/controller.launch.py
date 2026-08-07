c4522cbe453b92db8f2e4c7bc161110005e86a5f
Zaid <zedsargruroh@gmail.com>
2026-08-07 13:55:52 +0200

    added simple velocity controller based on robot kinematics

--- bumperbot_controller/launch/controller.launch.py
+++ bumperbot_controller/launch/controller.launch.py
@@ -1,8 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+
 
 def generate_launch_description():
     
+    use_python_arg = DeclareLaunchArgument(
+        "use_python", default_value="True"
+    )
+
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius", default_value="0.33"
+    )
+
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation", default_value="0.17"
+    )
+
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
+
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -23,7 +43,19 @@ def generate_launch_description():
         ]
     )
 
+    simple_controller_py = Node(
+        package="bumperbot_controller",
+        executable="simple_controller.py",
+        parameters=[{"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation}],
+        condition=IfCondition(use_python)
+    )
+
     return LaunchDescription([
+        use_python_arg,
+        wheel_radius_arg,
+        wheel_separation_arg,
         joint_state_broadcaster_spawner,
-        simple_controller
+        simple_controller,
+        simple_controller_py
     ])
