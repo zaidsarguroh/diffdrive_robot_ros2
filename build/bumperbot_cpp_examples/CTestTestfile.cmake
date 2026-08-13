49f19cb307f9e36f7f4eda396ad10603ff2b6017
Zaid Sarguroh <zedsarguroh@gmail.com>
2026-08-13 13:26:09 +0200

    Delete build directory

--- build/bumperbot_cpp_examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zaid/bumperbot_ws/src/bumperbot_cpp_examples
-# Build directory: /home/zaid/bumperbot_ws/src/build/bumperbot_cpp_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
