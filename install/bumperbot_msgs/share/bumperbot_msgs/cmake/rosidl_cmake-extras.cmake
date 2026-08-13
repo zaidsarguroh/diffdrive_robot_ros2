369d3814e45f0cca269ba69624b71b16d0e40aa0
Zaid Sarguroh <zedsarguroh@gmail.com>
2026-08-13 13:26:22 +0200

    Delete install directory

--- install/bumperbot_msgs/share/bumperbot_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(bumperbot_msgs_IDL_FILES "srv/AddTwoInts.idl;srv/GetTransform.idl")
-set(bumperbot_msgs_INTERFACE_FILES "srv/AddTwoInts.srv;srv/GetTransform.srv")
